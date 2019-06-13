051fe7f58c6a422f14a3e8f390326056c117c815
christianvogt <cvogt@redhat.com>
2019-06-13 15:49:21 -0400

    use jest types

--- frontend/public/declarations.d.ts
+++ frontend/public/declarations.d.ts
@@ -41,3 +41,8 @@ declare interface Window {
 // From https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html
 declare type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
 declare type Diff<T, K> = Omit<T, keyof K>;
+
+// Needed to prevent errors due to using jest types instead of jasmine.
+declare namespace jasmine {
+  function getEnv(): any;
+}
